2195e88417cd32c624a266a3bbf9df941f8069c3
Evav <Evav>
2024-10-23 18:30:42 +0800

    Seventh commit. a lot of changes related to index.php

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,14 +1,18 @@
 <header>
 
   <div class="head">
+
     <img src="{{asset('image/logotype.svg')}}" alt="image format png" />
-    <input type="text" placeholder="Поиск...">
+    <div class="flex gap-2">
+      <input type="text" placeholder="Поиск..."><button class="search"><img src="{{asset('image/search-icon.svg')}}" alt=""></button>
+    </div>
+
   </div>
 </header>
 
-<div class="under-header">
-  <a href="#">Главная</a>
-  <a href="#">Услуги</a>
-  <a href="#">Поддержка</a>
-  <a href="#">Логин</a>
+<div class="under-header min-w-9xl">
+  <a href="#" class="min-w-56 text-center border-x-1">Главная</a>
+  <a href="#" class="min-w-56 text-center border-x-1">Услуги</a>
+  <a href="#" class="min-w-56 text-center border-x-1">Поддержка</a>
+  <a href="#" class="min-w-56 text-center border-x-1">Логин</a>
 </div>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,13 +9,102 @@
 @section('content')
 
 <div class="main">
-    <p class="text-white">ТВОЙ ПЕРВЫЙ ХИТ НАЧНЕТСЯ ЗДЕСЬ</p>
+    <p class="text-white font-black">ТВОЙ ПЕРВЫЙ ХИТ НАЧНЕТСЯ ЗДЕСЬ</p>
     <button>эщкере</button>
 </div>
+<div class="trending">
+    <p class="text-white font-semibold">В тренде</p>
+    <div class="flex justify-center gap-8 my-5">
+        <div class="flex flex-col">
+        <img src="{{asset('image/image.png')}}" alt="">
+        <h2 class="text-white font-semibold my-5">HOPE - Pain lil Durk</h2>
+        <h3>Dj AIpach</h3>
+        <button class="text-white mt-2">В корзину</button>
+        </div>
+        <div class="flex flex-col">
+        <img src="{{asset('image/image-1.png')}}" alt="">
+        <h2 class="text-white font-semibold my-5">HOURS | DON TOLIVER</h2>
+        <h3>Prod. Boinovski</h3>
+        <button class="text-white mt-2">В корзину</button>
+        </div>
+        <div class="flex flex-col">
+        <img src="{{asset('image/image-2.png')}}" alt="">
+        <h2 class="text-white font-semibold my-5">FALL APART - Drake Type</h2>
+        <h3>Will Alexander</h3>
+        <button class="text-white mt-2">В корзину</button>
+        </div>
+        <div class="flex flex-col">
+        <img src="{{asset('image/image-3.png')}}" alt="">
+        <h2 class="text-white font-semibold my-5">BALENCIAGA 🔥2+1🔥</h2>
+        <h3>Kisses Beats</h3>
+        <button class="text-white mt-2">В корзину</button>
+        </div>
+        <div class="flex flex-col">
+        <img src="{{asset('image/image-4.png')}}" alt="">
+        <h2 class="text-white font-semibold my-5">21 Savage x A$ap Rocky</h2>
+        <h3>Hocii808</h3>
+        <button class="text-white mt-2">В корзину</button>
+        </div>
+        <div class="flex flex-col">
+        <img src="{{asset('image/image-5.png')}}" alt="">
+        <h2 class="text-white font-semibold my-5">PLAYERS - 1+9 FREE</h2>
+        <h3>Anywaywell</h3>
+        <button class="text-white mt-2">В корзину</button>
+        </div>
+        </div>
+</div>
 
-<!-- <div class="bask-p">
-    <p class="text-white">Получай доступ к трекам, которые ты не найдешь нигде Каждый ремикс сделан с душой и вниманием к деталям, чтобы порадовать даже самых взыскательных меломанов.</p>
-</div> -->
+<div class="trust">
+    <p class="text-center py-5">Нам доверяют:</p>
+    <div class="flex justify-center gap-12">
+        <img src="{{asset('image/sony.png')}}" alt="">
+        <img src="{{asset('image/elektra.png')}}" alt="">
+        <img src="{{asset('image/warner.png')}}" alt="">
+        <img src="{{asset('image/universal.png')}}" alt="">
+        <img src="{{asset('image/ea.png')}}" alt="">
+        <img src="{{asset('image/M.png')}}" alt="">
+        <img src="{{asset('image/300.png')}}" alt="">
+    </div>
+</div>
+
+<div class="info-block">
+    <p class="text-white font-extrabold">Начните свою музыкальную карьеру уже сегодня</p>
+    <div class="pt-3 block mx-auto">
+        <div class="flex">
+            <img class="block max-h-6" src="{{asset('image/mark.png')}}" alt="">
+
+            <div class="flex-chest flex-col">
+                <h2 class="text-white font-bold pl-3">Крупнейший рынок высококачественных ремиксов</h2>
+                <h3 class="pl-3">Получите доступ к более чем 322 миллионам битов от нашего растущего сообщества производителей по всему миру.</h3>
+            </div>
+        </div>
+        <div class="flex">
+            <img class="block max-h-6" src="{{asset('image/mark.png')}}" alt="">
+
+            <div class="flex-chest flex-col">
+                <h2 class="text-white font-bold pl-3">Бесперебойный процесс покупки</h2>
+                <h3 class="pl-3">Мы делаем это без особых усилий. Просматривайте свои любимые жанры и совершайте покупки с легкостью - и все это на одной платформе.</h3>
+            </div>
+        </div>
+        <div class="flex">
+            <img class="block max-h-6" src="{{asset('image/mark.png')}}" alt="">
+
+            <div class="flex-chest flex-col">
+                <h2 class="text-white font-bold pl-3">Простые варианты лицензирования</h2>
+                <h3 class="pl-3">Контракты не должны сбивать с толку. Тратьте меньше времени на то, чтобы ломать голову, и больше времени на создание вашего нового хита.</h3>
+            </div>
+        </div>
+        <div class="flex">
+            <img class="block max-h-6" src="{{asset('image/mark.png')}}" alt="">
+
+            <div class="flex-chest flex-col">
+                <h2 class="text-white font-bold pl-3">Сообщество, которое понимает вас</h2>
+                <h3 class="pl-3">Мы такие же создатели, как и вы. Независимо от того, нужна ли вам наша команда поддержки или вы хотите сотрудничать с креативщиками-единомышленниками, у нас есть для вас дом.</h3>
+            </div>
+        </div>
+    </div>
+</div>
+
 
 @endsection
 <!-- Секция с основным изменяемым содержимым -->
